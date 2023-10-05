f715999228659bc792022ff1c407202547fb4a53
karthik-3584 <146918408+karthik-3584@users.noreply.github.com>
2023-10-05 15:17:40 +0530

    Add files via upload

---
+++ python karthik.py
@@ -0,0 +1,36 @@
+class Satellite:
+    def __init__(self):
+        self.orientation = "North"
+        self.solar_panels = "Inactive"
+        self.data_collected = 0
+
+    def rotate(self, direction):
+        if direction in ["North", "South", "East", "West"]:
+            self.orientation = direction
+        else:
+            print("Invalid direction. Use North, South, East, or West.")
+    def activatePanels(self):
+        self.solar_panels = "Active"
+
+    def deactivatePanels(self):
+        self.solar_panels = "Inactive"
+
+    def collectData(self):
+        if self.solar_panels == "Active":
+            self.data_collected += 10
+        else:
+            print("Solar panels are inactive. Cannot collect data.")
+
+satellite = Satellite()
+n=int(input())
+for i in range(0,n):
+    satellite.rotate(input("Direction : "))
+    Pannel = input("pannel status : ")
+    if Pannel == "Activate":
+        satellite.activatePanels()
+    elif Pannel== "Ïnactivate":
+        satellite.deactivatePanels()
+    satellite.collectData()
+print(f"Orientation: {satellite.orientation}")
+print(f"Solar Panels: {satellite.solar_panels}")
+print(f"Data Collected: {satellite.data_collected}")
